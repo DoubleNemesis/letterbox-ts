48cb46177ee69b177bae4274da6c934bc56b0f4b
TomChant <chantaspell@gmail.com>
2021-12-02 23:46:42 +0000

    added handleGuess function

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-var wordToguess = "";
-var wordDisplayBox = document.getElementsByClassName('word-display-box')[0];
-var blanksDisplayBox = document.getElementsByClassName('blanks-display-box')[0];
-var livesDisplayBox = document.getElementsByClassName('lives-display-box')[0];
-var playerKeyboard = document.getElementsByClassName('player-keyboard')[0];
-var startBtn = document.getElementsByClassName('start-btn')[0];
-var resetBtn = document.getElementsByClassName('reset-btn')[0];
-var playerAttempt = "";
-var lives = 10;
-var livesEmoji = "&#127873;";
-var winMessage = "You Win! &#127881;";
-var loseMessage = "Game Over. &#128534;";
+let wordToGuess = "";
+const wordDisplayBox = document.getElementsByClassName('word-display-box')[0];
+const blanksDisplayBox = document.getElementsByClassName('blanks-display-box')[0];
+const livesDisplayBox = document.getElementsByClassName('lives-display-box')[0];
+const playerKeyboard = document.getElementsByClassName('player-keyboard')[0];
+const startBtn = document.getElementsByClassName('start-btn')[0];
+const resetBtn = document.getElementsByClassName('reset-btn')[0];
+let playerAttempt = "";
+let lives = 10;
+const livesEmoji = `&#127873;`;
+const winMessage = `You Win! &#127881;`;
+const loseMessage = `Game Over. &#128534;`;
 function createKeyboard() {
-    var unicodeA = 97;
-    var unicodeZ = 122;
-    for (var i = unicodeA; i <= unicodeZ; i++) {
-        var character = String.fromCharCode(i);
-        var letterButton = document.createElement('button');
+    const unicodeA = 97;
+    const unicodeZ = 122;
+    for (let i = unicodeA; i <= unicodeZ; i++) {
+        let character = String.fromCharCode(i);
+        let letterButton = document.createElement('button');
         letterButton.innerText = character;
         letterButton.className = 'letter-btn';
         letterButton.setAttribute('id', character);
@@ -25,8 +25,52 @@ function createKeyboard() {
     }
 }
 createKeyboard();
-function handleGuess() {
-}
 function handleStart() {
-    console.log('eho');
+    fetch('https://random-word-api.herokuapp.com/word?number=1&swear=0')
+        .then(response => response.json())
+        .then(data => {
+        console.log(typeof data);
+        wordToGuess = data.toString(); // change for testing
+        // wordDisplayBox.innerText = wordToGuess
+        for (let i = 0; i < wordToGuess.length; i++) {
+            playerAttempt += '-';
+        }
+        blanksDisplayBox.innerText = playerAttempt;
+        startBtn.style.display = 'none';
+        resetBtn.style.display = 'block';
+        // livesDisplayBox.innerText = `${lives} Lives Left`
+        renderLivesLeftEmojis();
+        const letterButtons = Array.from(document.getElementsByClassName('letter-btn'));
+        letterButtons.forEach((letterButton) => {
+            letterButton.disabled = false;
+        });
+    });
 }
+function handleGuess(e) {
+    console.log(e.target.id);
+    console.log(e);
+    const stringToCheck = e.target.id;
+    const targetBtn = document.getElementById(e.target.id);
+    targetBtn.classList.add('used');
+    targetBtn.disabled = true;
+    if (wordToGuess.indexOf(stringToCheck) > -1) {
+        for (let i = 0; i < wordToGuess.length; i++) {
+            if (stringToCheck === wordToGuess[i]) {
+                let playerAttemptArray = playerAttempt.split('');
+                playerAttemptArray[i] = stringToCheck;
+                playerAttempt = playerAttemptArray.join('');
+            }
+        }
+        // checkWin()
+    }
+    else {
+        lives--;
+        // livesDisplayBox.innerText = `${lives} Lives Left`
+        renderLivesLeftEmojis();
+        // checkLose()
+    }
+    blanksDisplayBox.innerText = playerAttempt;
+}
+function renderLivesLeftEmojis() {
+}
+//# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,10 +1,10 @@
-let wordToguess:string = ""
-const wordDisplayBox = document.getElementsByClassName('word-display-box')[0]
-const blanksDisplayBox = document.getElementsByClassName('blanks-display-box')[0]
-const livesDisplayBox = document.getElementsByClassName('lives-display-box')[0]
-const playerKeyboard = document.getElementsByClassName('player-keyboard')[0]
-const startBtn = document.getElementsByClassName('start-btn')[0]
-const resetBtn = document.getElementsByClassName('reset-btn')[0]
+let wordToGuess:string = ""
+const wordDisplayBox = document.getElementsByClassName('word-display-box')[0] as HTMLElement
+const blanksDisplayBox = document.getElementsByClassName('blanks-display-box')[0] as HTMLElement
+const livesDisplayBox = document.getElementsByClassName('lives-display-box')[0] as HTMLElement
+const playerKeyboard = document.getElementsByClassName('player-keyboard')[0] as HTMLElement
+const startBtn = document.getElementsByClassName('start-btn')[0] as HTMLElement
+const resetBtn = document.getElementsByClassName('reset-btn')[0] as HTMLElement
 let playerAttempt:string = ""
 let lives:number = 10
 const livesEmoji:string = `&#127873;`
@@ -29,9 +29,55 @@ function createKeyboard():void{
 }
 createKeyboard()
 
-function handleGuess(){
+function handleStart():void{
+    fetch('https://random-word-api.herokuapp.com/word?number=1&swear=0')
+    .then(response => response.json())
+    .then(data => {
+        console.log(typeof data)      
+        wordToGuess = data.toString()  // change for testing
+        // wordDisplayBox.innerText = wordToGuess
+        for (let i = 0; i < wordToGuess.length; i++) {
+            playerAttempt += '-'
+        }
+        blanksDisplayBox.innerText = playerAttempt
+        startBtn.style.display = 'none'
+        resetBtn.style.display = 'block'
+        // livesDisplayBox.innerText = `${lives} Lives Left`
+        renderLivesLeftEmojis()
+        const letterButtons = Array.from(document.getElementsByClassName('letter-btn') as HTMLCollection)
+        letterButtons.forEach((letterButton)=>{
+            (letterButton as HTMLInputElement).disabled = false
+        })
+    })
 }
 
-function handleStart():void{
-    console.log('eho')
+function handleGuess(e:PointerEvent):void{
+    console.log((<HTMLButtonElement>e.target).id) 
+    console.log(e) 
+    const stringToCheck:string = (<HTMLButtonElement>e.target).id
+    const targetBtn:HTMLInputElement = document.getElementById((<HTMLButtonElement>e.target).id) as HTMLInputElement
+    targetBtn.classList.add('used')
+    targetBtn.disabled = true
+    if (wordToGuess.indexOf(stringToCheck) > -1) {
+        for (let i = 0; i < wordToGuess.length; i++) {
+            if (stringToCheck === wordToGuess[i]) {
+                let playerAttemptArray = playerAttempt.split('')
+                playerAttemptArray[i] = stringToCheck
+                playerAttempt = playerAttemptArray.join('')
+            }
+        }
+        // checkWin()
+    }
+    else {
+        lives--
+        // livesDisplayBox.innerText = `${lives} Lives Left`
+        renderLivesLeftEmojis()
+        // checkLose()
+    }
+    blanksDisplayBox.innerText = playerAttempt
+}
+
+
+function renderLivesLeftEmojis(){
+
 }
